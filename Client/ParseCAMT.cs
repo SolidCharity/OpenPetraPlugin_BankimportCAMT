d6d37c65f6ae8b0fbc1aa5c3e69a4e703be69e35
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2015-11-02 12:45:17 +0100

    implement parsing the CAMT xml files. add code for testing.

--- Client/ParseCAMT.cs
+++ Client/ParseCAMT.cs
@@ -36,6 +36,7 @@ namespace Ict.Petra.Plugins.BankimportCAMT.Client
     /// for the structure of the file see
     /// https://www.rabobank.com/nl/images/Format%20description%20CAMT.053.pdf
     /// http://www.national-bank.de/fileadmin/user_upload/nationalbank/Service_Center/Electronic_Banking_Center/Downloads/Handbuecher_und_Bedingungen/SRZ-Anlage_5b_Kontoauszug_ISO_20022_camt_2010-06-15b.pdf
+    /// http://www.hettwer-beratung.de/sepa-spezialwissen/sepa-technische-anforderungen/camt-format-camt-053/
     /// </summary>
     public class TCAMTParser
     {
@@ -69,46 +70,114 @@ namespace Ict.Petra.Plugins.BankimportCAMT.Client
 
             try
             {
-                TStatement stmt = new TStatement();
                 XmlDocument doc = new XmlDocument();
                 doc.Load(filename);
-                XmlNode nodeDocument = doc.SelectSingleNode("/Document");
-                if (nodeDocument.Attributes["xmlns"].Value != "urn:iso:std:iso:20022:tech:xsd:camt.053.001.02")
+                string namespaceName = "urn:iso:std:iso:20022:tech:xsd:camt.053.001.02";
+                XmlNamespaceManager nsmgr = new XmlNamespaceManager(doc.NameTable);
+                nsmgr.AddNamespace("camt", namespaceName);
+
+                XmlNode nodeDocument = doc.DocumentElement;
+
+                if ((nodeDocument == null) || (nodeDocument.Attributes["xmlns"].Value != namespaceName))
                 {
-                    throw new Exception("expecting xmlns = 'urn:iso:std:iso:20022:tech:xsd:camt.053.001.02'");
+                    throw new Exception("expecting xmlns = '" + namespaceName + "'");
                 }
 
-                XmlNode nodeStatement = doc.SelectSingleNode("/Document/Stmt");
-                stmt.accountCode = nodeStatement.SelectSingleNode("/Acct/Id/IBAN").Value;
-                stmt.bankCode = nodeStatement.SelectSingleNode("/Acct/Svcr/FinInstnId/BIC").Value;
-                stmt.currency = nodeStatement.SelectSingleNode("/Acct/Ccy").Value;
-                XmlNode nodeBalances = nodeStatement.SelectNodes("/Bal");
-                foreach (XmlNode nodeBalance in nodeBalances)
+                XmlNodeList stmts = nodeDocument.SelectNodes("camt:BkToCstmrStmt/camt:Stmt", nsmgr);
+
+                foreach (XmlNode nodeStatement in stmts)
                 {
-                    // PRCD: PreviouslyClosedBooked
-                    if (nodeBalance.SelectSingleNode("/Tp/CdOrPrtry/Cd").Value == "PRCD")
+                    TStatement stmt = new TStatement();
+                    currentStatement = stmt;
+
+                    stmt.id = nodeStatement.SelectSingleNode("camt:Id", nsmgr).InnerText;
+                    stmt.accountCode = nodeStatement.SelectSingleNode("camt:Acct/camt:Id/camt:IBAN", nsmgr).InnerText;
+                    stmt.bankCode = nodeStatement.SelectSingleNode("camt:Acct/camt:Svcr/camt:FinInstnId/camt:BIC", nsmgr).InnerText;
+                    stmt.currency = nodeStatement.SelectSingleNode("camt:Acct/camt:Ccy", nsmgr).InnerText;
+                    string ownName = nodeStatement.SelectSingleNode("camt:Acct/camt:Ownr/camt:Nm", nsmgr).InnerText;
+                    XmlNodeList nodeBalances = nodeStatement.SelectNodes("camt:Bal", nsmgr);
+
+                    foreach (XmlNode nodeBalance in nodeBalances)
                     {
-                        stmt.startBalance = Decimal.Parse(nodeBalance.SelectSingleNode("/Amt").Value);
-                        // CreditDebitIndicator: CRDT or DBIT for credit or debit
-                        if (nodeBalance.SelectSingleNode("/CdtDbtInd") == "DBIT")
+                        // PRCD: PreviouslyClosedBooked
+                        if (nodeBalance.SelectSingleNode("camt:Tp/camt:CdOrPrtry/camt:Cd", nsmgr).InnerText == "PRCD")
                         {
-                            stmt.startBalance *= -1.0m;
+                            stmt.startBalance = Decimal.Parse(nodeBalance.SelectSingleNode("camt:Amt", nsmgr).InnerText);
+
+                            // CreditDebitIndicator: CRDT or DBIT for credit or debit
+                            if (nodeBalance.SelectSingleNode("camt:CdtDbtInd", nsmgr).InnerText == "DBIT")
+                            {
+                                stmt.startBalance *= -1.0m;
+                            }
                         }
+                        // CLBD: ClosingBooked
+                        else if (nodeBalance.SelectSingleNode("camt:Tp/camt:CdOrPrtry/camt:Cd", nsmgr).InnerText == "CLBD")
+                        {
+                            stmt.endBalance = Decimal.Parse(nodeBalance.SelectSingleNode("camt:Amt", nsmgr).InnerText);
+
+                            // CreditDebitIndicator: CRDT or DBIT for credit or debit
+                            if (nodeBalance.SelectSingleNode("camt:CdtDbtInd", nsmgr).InnerText == "DBIT")
+                            {
+                                stmt.endBalance *= -1.0m;
+                            }
+
+                            stmt.date = DateTime.Parse(nodeBalance.SelectSingleNode("camt:Dt", nsmgr).InnerText);
+                        }
+
+                        // ITBD: InterimBooked
+                        // CLAV: ClosingAvailable
+                        // FWAV: ForwardAvailable
                     }
-                    // CLBD: ClosingBooked
-                    else if (nodeBalance.SelectSingleNode("/Tp/CdOrPrtry/Cd").Value == "CLBD")
+
+                    XmlNodeList nodeEntries = nodeStatement.SelectNodes("camt:Ntry", nsmgr);
+
+                    foreach (XmlNode nodeEntry in nodeEntries)
                     {
-                        stmt.endBalance = Decimal.Parse(nodeBalance.SelectSingleNode("/Amt").Value);
-                        // CreditDebitIndicator: CRDT or DBIT for credit or debit
-                        if (nodeBalance.SelectSingleNode("/CdtDbtInd") == "DBIT")
+                        TTransaction tr = new TTransaction();
+                        tr.inputDate = DateTime.Parse(nodeEntry.SelectSingleNode("camt:BookgDt/camt:Dt", nsmgr).InnerText);
+                        tr.valueDate = DateTime.Parse(nodeEntry.SelectSingleNode("camt:ValDt/camt:Dt", nsmgr).InnerText);
+                        tr.amount = Decimal.Parse(nodeEntry.SelectSingleNode("camt:Amt", nsmgr).InnerText);
+
+                        if (nodeEntry.SelectSingleNode("camt:Amt", nsmgr).Attributes["Ccy"].Value != stmt.currency)
                         {
-                            stmt.endBalance *= -1.0m;
+                            throw new Exception("transaction currency " + nodeEntry.SelectSingleNode("camt:Amt",
+                                    nsmgr).Attributes["Ccy"].Value + " does not match the bank statement currency");
                         }
-                        stmt.date = DateTime.Parse(nodeBalance.SelectSingleNode("/Dt/Dt").Value);
+
+                        if (nodeEntry.SelectSingleNode("camt:CdtDbtInd", nsmgr).InnerText == "DBIT")
+                        {
+                            tr.amount *= -1.0m;
+                        }
+
+                        tr.description = nodeEntry.SelectSingleNode("camt:NtryDtls/camt:TxDtls/camt:RmtInf/camt:Ustrd", nsmgr).InnerText;
+                        XmlNode partnerName = nodeEntry.SelectSingleNode("camt:NtryDtls/camt:TxDtls/camt:RltdPties/camt:Dbtr/camt:Nm", nsmgr);
+
+                        if (partnerName != null)
+                        {
+                            tr.partnerName = partnerName.InnerText;
+                        }
+
+                        XmlNode accountCode = nodeEntry.SelectSingleNode("camt:NtryDtls/camt:TxDtls/camt:RltdPties/camt:DbtrAcct/camt:Id/camt:IBAN",
+                            nsmgr);
+
+                        if (accountCode != null)
+                        {
+                            tr.accountCode = accountCode.InnerText;
+                        }
+
+                        XmlNode CrdtName = nodeEntry.SelectSingleNode("camt:NtryDtls/camt:TxDtls/camt:RltdPties/camt:Cdtr/camt:Nm", nsmgr);
+
+                        if ((CrdtName != null) && (CrdtName.InnerText != ownName))
+                        {
+                            // sometimes donors write the project or recipient in the field where the organisation is supposed to be
+                            tr.description += " " + CrdtName.InnerText;
+                        }
+
+                        tr.typecode = nodeEntry.SelectSingleNode("camt:Sts", nsmgr).InnerText;
+                        stmt.transactions.Add(tr);
+
+                        TLogging.LogAtLevel(2, "count : " + stmt.transactions.Count.ToString());
                     }
-                    // ITBD: InterimBooked
-                    // CLAV: ClosingAvailable
-                    // FWAV: ForwardAvailable
                 }
             }
             catch (Exception e)
